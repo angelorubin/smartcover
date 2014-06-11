6e9e58f4855e77d4ab663c8468e8ec528edd8248
angelorubin <angelorubin@gmail.com>
2014-06-11 19:37:01 -0300

    pagina personalização montagem capa

---
+++ app/views/master/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>SmartCover</title>
+	{{ HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css') }}
+	{{ HTML::script('//maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js') }}
+</head>
+<body>
+	<button type="button" class="btn btn-primary">Primary</button>
+	<h1>Monte sua capa</h1>
+	<hr/>
+	<select>
+		<option>Um</option>
+		<option>Dois</option>
+		<option>Três</option>
+	</select>
+</body>
+</html>
